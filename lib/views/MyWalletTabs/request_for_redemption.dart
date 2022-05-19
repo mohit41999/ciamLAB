ac4b1e4391dfcb26688f1b2444a62885dbad87aa
mohit <mohit41999@gmail.com>
2022-05-19 19:24:27 +0530

    lab

--- lib/views/MyWalletTabs/request_for_redemption.dart
+++ lib/views/MyWalletTabs/request_for_redemption.dart
@@ -1,7 +1,6 @@
 import 'package:ciam_lab/Utils/colorsandstyles.dart';
 import 'package:ciam_lab/widgets/common_button.dart';
 import 'package:ciam_lab/widgets/title_enter_field.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:video_player/video_player.dart';
 
 import 'biometric_authenticate2.dart';
 import 'home_screen.dart';
@@ -19,11 +20,13 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  late Timer timer;
+  late VideoPlayerController controller;
   Future<void> navigate() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     print(prefs.get('user_id'));
-    Timer(
-        Duration(seconds: 3),
+    timer = Timer(
+        Duration(seconds: 4),
         () => (prefs.get('user_id').toString() == null.toString())
             ? Navigator.pushReplacement(context,
                 MaterialPageRoute(builder: (context) => SignInScreen()))
@@ -37,8 +40,19 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   FireBaseSetup fireBaseSetup = FireBaseSetup();
+  loadVideoPlayer() {
+    controller = VideoPlayerController.asset('assets/video/splash.mp4');
+
+    controller.initialize().then((value) {
+      setState(() {
+        controller.play();
+      });
+    });
+  }
+
   @override
   void initState() {
+    loadVideoPlayer();
     navigate();
     fireBaseSetup.storefcmToken();
 
@@ -46,88 +60,98 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    print('dispose callleddd');
+    controller.dispose();
+    timer.cancel();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Align(
-              alignment: Alignment.topLeft,
-              child: Container(
-                height: 200,
-                width: 150,
-                decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                          color: Color(0xffFF2442),
-                          blurRadius: 20,
-                          spreadRadius: 40)
-                    ],
-                    color: Color(0xffFF2442),
-                    borderRadius:
-                        BorderRadius.only(bottomRight: Radius.circular(200))),
-              )),
-          Align(
-              alignment: Alignment.topRight,
-              child: Container(
-                height: 200,
-                width: 200,
-                decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                          color: Color(0xffFFC069),
-                          blurRadius: 20,
-                          spreadRadius: 15)
-                    ],
-                    color: Color(0xffFFC069),
-                    borderRadius:
-                        BorderRadius.only(bottomLeft: Radius.circular(200))),
-              )),
-          Align(
-              alignment: Alignment.bottomLeft,
-              child: Container(
-                height: 180,
-                width: 180,
-                decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                          color: Color(0xff085385),
-                          blurRadius: 20,
-                          spreadRadius: 30)
-                    ],
-                    color: Color(0xff085385),
-                    borderRadius:
-                        BorderRadius.only(topRight: Radius.circular(200))),
-              )),
-          Align(
-              alignment: Alignment.centerRight,
-              child: Container(
-                height: 200,
-                width: 80,
-                decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                          color: Color(0xff9A671A),
-                          blurRadius: 20,
-                          spreadRadius: 30)
-                    ],
-                    color: Color(0xff9A671A).withOpacity(0.1),
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(200),
-                        bottomLeft: Radius.circular(200))),
-              )),
-          Container(
-            width: double.infinity,
-            height: double.infinity,
-            decoration: BoxDecoration(boxShadow: [
-              BoxShadow(
-                  color: Colors.white.withOpacity(0.7),
-                  blurRadius: 10,
-                  spreadRadius: 20)
-            ]),
-            child: Center(child: Image.asset('assets/pngs/logo.png')),
-          )
-        ],
-      ),
+      body: VideoPlayer(controller),
+      // Stack(
+      //   children: [
+      //     Align(
+      //         alignment: Alignment.topLeft,
+      //         child: Container(
+      //           height: 200,
+      //           width: 150,
+      //           decoration: BoxDecoration(
+      //               boxShadow: [
+      //                 BoxShadow(
+      //                     color: Color(0xffFF2442),
+      //                     blurRadius: 20,
+      //                     spreadRadius: 40)
+      //               ],
+      //               color: Color(0xffFF2442),
+      //               borderRadius:
+      //                   BorderRadius.only(bottomRight: Radius.circular(200))),
+      //         )),
+      //     Align(
+      //         alignment: Alignment.topRight,
+      //         child: Container(
+      //           height: 200,
+      //           width: 200,
+      //           decoration: BoxDecoration(
+      //               boxShadow: [
+      //                 BoxShadow(
+      //                     color: Color(0xffFFC069),
+      //                     blurRadius: 20,
+      //                     spreadRadius: 15)
+      //               ],
+      //               color: Color(0xffFFC069),
+      //               borderRadius:
+      //                   BorderRadius.only(bottomLeft: Radius.circular(200))),
+      //         )),
+      //     Align(
+      //         alignment: Alignment.bottomLeft,
+      //         child: Container(
+      //           height: 180,
+      //           width: 180,
+      //           decoration: BoxDecoration(
+      //               boxShadow: [
+      //                 BoxShadow(
+      //                     color: Color(0xff085385),
+      //                     blurRadius: 20,
+      //                     spreadRadius: 30)
+      //               ],
+      //               color: Color(0xff085385),
+      //               borderRadius:
+      //                   BorderRadius.only(topRight: Radius.circular(200))),
+      //         )),
+      //     Align(
+      //         alignment: Alignment.centerRight,
+      //         child: Container(
+      //           height: 200,
+      //           width: 80,
+      //           decoration: BoxDecoration(
+      //               boxShadow: [
+      //                 BoxShadow(
+      //                     color: Color(0xff9A671A),
+      //                     blurRadius: 20,
+      //                     spreadRadius: 30)
+      //               ],
+      //               color: Color(0xff9A671A).withOpacity(0.1),
+      //               borderRadius: BorderRadius.only(
+      //                   topLeft: Radius.circular(200),
+      //                   bottomLeft: Radius.circular(200))),
+      //         )),
+      //     Container(
+      //       width: double.infinity,
+      //       height: double.infinity,
+      //       decoration: BoxDecoration(boxShadow: [
+      //         BoxShadow(
+      //             color: Colors.white.withOpacity(0.7),
+      //             blurRadius: 10,
+      //             spreadRadius: 20)
+      //       ]),
+      //       child: Center(child: Image.asset('assets/pngs/logo.png')),
+      //     )
+      //   ],
+      // ),
     );
   }
 }
